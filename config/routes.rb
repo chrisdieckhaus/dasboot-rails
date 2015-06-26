732035f647d9e49e64302e1c5094a060f5a9bbe1
Chris Dieckhaus <christopherdieckhaus@gmail.com>
2015-06-26 13:20:03 -0400

    Add user login

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   get 'signup' => 'users#new'
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
   resources :users
 
   # You can have the root of your site routed with "root"
