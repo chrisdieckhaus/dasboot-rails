0bb35ffdc07eab2ed8d35dcb01635bd5bd89faf2
Chris Dieckhaus <christopherdieckhaus@gmail.com>
2015-07-07 14:09:24 -0400

    Generate Schedule Model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150703025342) do
+ActiveRecord::Schema.define(version: 20150707180621) do
 
   create_table "availabilities", force: :cascade do |t|
     t.date     "day_date"
@@ -33,6 +33,14 @@ ActiveRecord::Schema.define(version: 20150703025342) do
   end
 
   add_index "days", ["availability_id"], name: "index_days_on_availability_id"
+
+  create_table "schedules", force: :cascade do |t|
+    t.date     "start_date"
+    t.date     "end_date"
+    t.string   "schedule_name"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
