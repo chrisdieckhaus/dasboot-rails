bb3442574fbfd2d69e955a127316d4b607b11a12
Chris Dieckhaus <christopherdieckhaus@gmail.com>
2015-07-09 15:05:06 -0400

    Implement edit shifts

--- app/controllers/shifts_controller.rb
+++ app/controllers/shifts_controller.rb
@@ -12,15 +12,37 @@ class ShiftsController < ApplicationController
 		date = Date.new shift_p["start_time(1i)"].to_i, shift_p["start_time(2i)"].to_i, shift_p["start_time(3i)"].to_i
 		start_time = DateTime.new shift_p["start_time(1i)"].to_i, shift_p["start_time(2i)"].to_i, shift_p["start_time(3i)"].to_i, shift_p["start_time(4i)"].to_i, shift_p["start_time(5i)"].to_i
 		end_time = DateTime.new shift_p["end_time(1i)"].to_i, shift_p["end_time(2i)"].to_i, shift_p["end_time(3i)"].to_i, shift_p["end_time(4i)"].to_i, shift_p["end_time(5i)"].to_i
+		good_times = end_time >= start_time
 		@shift.start_time = start_time
 		@shift.end_time = end_time
 		@shift.user_id = shift_p["user_id"]
 		@shift.schedule_id = shift_p["schedule_id"]
-		@shift.save
-		if @shift.save 
+		if good_times && @shift.save
 			redirect_to schedule_path(shift_p['schedule_id'])
 		else 
-			redirect_to schedule_shifts_new_path(shift_p['schedule_id'])
+			redirect_to schedule_shifts_new_path(shift_p['schedule_id']), notice: 'Error.'
+		end 
+	end
+
+	def edit
+		@shift = Shift.find(params[:id])
+	end
+
+	def update
+		@shift = Shift.find(params[:id])
+		shift_p = params[:shift]
+		date = Date.new shift_p["start_time(1i)"].to_i, shift_p["start_time(2i)"].to_i, shift_p["start_time(3i)"].to_i
+		start_time = DateTime.new shift_p["start_time(1i)"].to_i, shift_p["start_time(2i)"].to_i, shift_p["start_time(3i)"].to_i, shift_p["start_time(4i)"].to_i, shift_p["start_time(5i)"].to_i
+		end_time = DateTime.new shift_p["end_time(1i)"].to_i, shift_p["end_time(2i)"].to_i, shift_p["end_time(3i)"].to_i, shift_p["end_time(4i)"].to_i, shift_p["end_time(5i)"].to_i
+		good_times = end_time >= start_time
+		@shift.start_time = start_time
+		@shift.end_time = end_time
+		@shift.user_id = shift_p["user_id"]
+		@shift.schedule_id = shift_p["schedule_id"]
+		if good_times && @shift.save
+			redirect_to schedule_path(shift_p['schedule_id'])
+		else 
+			redirect_to edit_shift_path(shift_p['schedule_id']), notice: 'Error.'
 		end 
 	end
 
--- app/models/shift.rb
+++ app/models/shift.rb
@@ -1,4 +1,5 @@
 class Shift < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :schedule
+	validates_presence_of :user_id, :message => 'Please select a member'
 end
