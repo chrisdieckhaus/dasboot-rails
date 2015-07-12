d8e3f40a8da63e07392c9886c0b0652957088614
Chris <christopherdieckhaus@gmail.com>
2015-07-12 14:53:06 -0400

    Change fields on availabilities to datetimes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150710194311) do
+ActiveRecord::Schema.define(version: 20150712182432) do
 
   create_table "availabilities", force: :cascade do |t|
     t.date     "day_date"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 20150710194311) do
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "start_time"
+    t.datetime "end_time"
   end
 
   add_index "availabilities", ["user_id"], name: "index_availabilities_on_user_id"
