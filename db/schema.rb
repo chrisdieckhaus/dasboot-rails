e3def849b82f9467108dccd97b44015a277dc02e
Chris <christopherdieckhaus@gmail.com>
2015-07-12 14:58:04 -0400

    Remove times column from Availabilities

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150712182432) do
+ActiveRecord::Schema.define(version: 20150712185655) do
 
   create_table "availabilities", force: :cascade do |t|
     t.date     "day_date"
