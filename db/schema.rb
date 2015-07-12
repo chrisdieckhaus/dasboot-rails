2fefeb360a4336ed50d38d348f2deb8f694c2842
Chris <christopherdieckhaus@gmail.com>
2015-07-12 14:20:00 -0400

    Alter time select options

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150710202733) do
+ActiveRecord::Schema.define(version: 20150710194311) do
 
   create_table "availabilities", force: :cascade do |t|
     t.date     "day_date"
@@ -25,11 +25,10 @@ ActiveRecord::Schema.define(version: 20150710202733) do
 
   create_table "days", force: :cascade do |t|
     t.integer  "availability_id"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-    t.string   "day_name"
     t.date     "day_date"
     t.text     "times"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
   add_index "days", ["availability_id"], name: "index_days_on_availability_id"
