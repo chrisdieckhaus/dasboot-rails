ef19a18fc292cbbe209f92edf6e6d3010afc281a
Chris Dieckhaus <christopherdieckhaus@gmail.com>
2015-07-15 10:06:37 -0400

    Begin styling login page

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,6 +5,7 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( bootstrap.css )
 Rails.application.config.assets.precompile += %w( jquery.js )
 Rails.application.config.assets.precompile += %w( header.css )
+Rails.application.config.assets.precompile += %w( sessions.css )
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
