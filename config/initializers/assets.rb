c3b2b6e157da8ae82e5267a1910594c741b54609
Chris <christopherdieckhaus@gmail.com>
2015-07-26 20:44:12 -0400

    Precompile app.css

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,6 +9,7 @@ Rails.application.config.assets.precompile += %w( sessions.css )
 Rails.application.config.assets.precompile += %w( static.css )
 Rails.application.config.assets.precompile += %w( contact.css )
 Rails.application.config.assets.precompile += %w( members.css )
+Rails.application.config.assets.precompile += %w( app.css )
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
